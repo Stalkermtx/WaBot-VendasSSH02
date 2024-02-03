c2f058b957b2c0070200b2c0fbfd65b7215faed4
Stalkermtx <106044847+Stalkermtx@users.noreply.github.com>
2024-02-03 18:59:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,21 +9,21 @@
 }
 apt update -y
 echo
-echo "Instalando bot, aguarde..."
+echo "Instalando Bot, Aguarde..."
 echo
 apt install nodejs -y > /dev/null 2>&1
 apt install unzip -y > /dev/null 2>&1
 apt install screen -y > /dev/null 2>&1
 apt install wget -y > /dev/null 2>&1
-wget https://raw.githubusercontent.com/endblack/WaBot-VendasSSH/main/mult/onbot > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1
+wget https://raw.githubusercontent.com/Stalkermtx/WaBot-VendasSSH/main/mult/onbot > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/raw/main/mult/offbot > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/raw/main/mult/qrcode > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/raw/main/mult/config.js > /dev/null 2>&1
 chmod +x onbot offbot qrcode
 mv onbot offbot qrcode /bin
-wget https://github.com/endblack/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
+wget https://github.com/Stalkermtx/WaBot-VendasSSH/raw/main/mult/megahbot.zip -O /etc/megahbot.zip > /dev/null 2>&1
 unzip /etc/megahbot.zip
 mv megahbot /etc
 echo
-echo "Instalação terminada! Não esqueça de editar seus dados no arquivo config.js"
+echo "Instalação do Bot Terminada! Não Esqueça de Editar Seus Dados no Arquivo Config.js"
 echo
